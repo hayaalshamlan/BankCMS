08c5456ace6c648a18c840b5ee47aba31e0adbdb
83336 <halshamlan@yahoo.com>
2024-04-16 10:24:27 +0300

    Add project files.

---
+++ BankCMS/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using BankCMS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankCMS.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+     List<Customer> customers = new List<Customer>();
+           
+            {
+                new Customer { Id = 0080, Name = "haya", Email = "haya@gmail.com" };
+           
+
+            };
+            return View(customers);
+        }
+    }
+}
